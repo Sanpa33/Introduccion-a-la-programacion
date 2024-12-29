668e3bca0c5e08321f45bfb9249455c5ceb797e1
Sanpa33 <127140452+Sanpa33@users.noreply.github.com>
2024-12-28 22:21:37 -0300

    Termina ejercicios 1 al 5

--- Practicas/Practica6/practica6.py
+++ Practicas/Practica6/practica6.py
@@ -1,3 +1,17 @@
+import math
+
 def imprimir_hola_mundo():
     return "Hola mundo!"
+
+def imprimir_un_verso():
+    return print("En este film velado en blanca noche\nEl hijo tenaz de tu enemigo\nEl muy verdugo cena distinguido\nUna noche de cristal que se hace añicos")
+
+def raizDe2():
+    return round(math.sqrt(2),2)
+
+def factorial_de_dos():
+    return 2 * 1
+
+def perimetro():
+    return 2 * math.pi
 
