5fbdfe002d83fdd401bca09a8f3205af0aecd686
Sanpa33 <127140452+Sanpa33@users.noreply.github.com>
2024-12-29 21:55:11 -0300

    Agrega ejercicio 2 problemas 1-5

--- Practicas/Practica6/practica6.py
+++ Practicas/Practica6/practica6.py
@@ -1,5 +1,7 @@
 import math
 
+##Ejercicio 1
+
 def imprimir_hola_mundo():
     return "Hola mundo!"
 
@@ -14,4 +16,23 @@ def factorial_de_dos():
 
 def perimetro():
     return 2 * math.pi
+
+######################################################################################################
+
+#Ejercicio 2
+
+def imprimir_saludo(nombre:str) -> str:
+    return (f"Hola {nombre}")
+
+def raiz_cuadrada_de(numero:int) -> float:
+    return math.sqrt(numero)
+
+def fahrenheit_a_celsius(temp_far:float) -> float:
+    return ((temp_far - 32) *5) / 9
+
+def imprimir_dos_veces(estribillo:str) -> str:
+    return (estribillo + " ") * 2
+
+def es_multiplo(n:int,m:int) -> bool:
+    return (n % m) == 0
 
