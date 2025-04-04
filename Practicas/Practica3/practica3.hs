4b828c9fd92fd1d804068036d1c26b016b434e05
Sanpa33 <127140452+Sanpa33@users.noreply.github.com>
2025-04-03 23:13:48 -0300

    Agrega pdf practica y ejercicio 1

---
+++ Practicas/Practica3/practica3.hs
@@ -0,0 +1,22 @@
+--Ejercicio 1
+f :: Integer -> Integer
+f 1 = 8
+f 4 = 131
+f 16 = 16
+
+--B)
+
+g :: Integer -> Integer
+g 8 = 16
+g 16 = 4
+g 131 = 16
+
+
+--C)
+
+h :: Integer -> Integer
+h x = f (g x)
+
+k :: Integer -> Integer
+k x = g (f x)
+
