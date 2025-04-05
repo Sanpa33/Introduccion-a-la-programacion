caae2bd4c6e45008b9e56b694b4a907e2efb8ad7
Sanpa33 <127140452+Sanpa33@users.noreply.github.com>
2025-04-05 13:15:17 -0300

    Agrega Ejercicio 2 a-b-c

--- Practicas/Practica3/practica3.hs
+++ Practicas/Practica3/practica3.hs
@@ -1,3 +1,5 @@
+import System.Win32 (xBUTTON1, cOLOR_ACTIVEBORDER)
+import Text.XHtml (above)
 --Ejercicio 1
 f :: Integer -> Integer
 f 1 = 8
@@ -20,3 +22,26 @@ h x = f (g x)
 k :: Integer -> Integer
 k x = g (f x)
 
+--2)
+
+--A)
+
+absoluto :: Integer -> Integer
+absoluto x
+            | x < 0 = -x
+            | otherwise = x
+
+--B)    
+
+maximoAbsoluto :: Integer -> Integer -> Integer
+maximoAbsoluto x y
+            | absoluto x > absoluto y = absoluto x
+            | otherwise = absoluto y
+
+--C)
+
+maximo3 :: Integer -> Integer -> Integer -> Integer
+maximo3 a b c 
+            | (a >= b) && (a >= c) = a 
+            | (b >= a) && (b >= c) = b 
+            | (c >= a) && (c >= b) = c
