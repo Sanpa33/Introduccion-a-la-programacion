0e5888c98e77bc78b439e81ad45d2bb8eda8014b
Sanpa33 <127140452+Sanpa33@users.noreply.github.com>
2025-04-05 13:52:43 -0300

    Agrega ejercicio D y E

--- Practicas/Practica3/practica3.hs
+++ Practicas/Practica3/practica3.hs
@@ -1,5 +1,6 @@
 import System.Win32 (xBUTTON1, cOLOR_ACTIVEBORDER)
 import Text.XHtml (above)
+import Distribution.SPDX (LicenseId(TORQUE_1_1))
 --Ejercicio 1
 f :: Integer -> Integer
 f 1 = 8
@@ -45,3 +46,28 @@ maximo3 a b c
             | (a >= b) && (a >= c) = a 
             | (b >= a) && (b >= c) = b 
             | (c >= a) && (c >= b) = c
+
+--D)
+
+algunoEsCero :: Float -> Float -> Bool
+algunoEsCero x y = (x==0) || (y==0) 
+
+-- Con Pattern Matching
+
+algunoEsCero2 :: Float -> Float -> Bool
+algunoEsCero2 0 _ = True
+algunoEsCero2 _ 0 = True
+algunoEsCero2 _ _ = False
+
+--E)
+
+ambasSonCero :: Float -> Float -> Bool
+ambasSonCero x y = (x==y) && (x==y)
+
+-- Con Pattern Matching
+
+ambosSonCero2 :: Float -> Float -> Bool
+ambosSonCero2 0 0 = True
+ambosSonCero2 0 _ = False
+ambosSonCero2 _ 0 = False
+
