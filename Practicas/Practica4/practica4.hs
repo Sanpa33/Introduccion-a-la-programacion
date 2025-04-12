9b08785749713b9dd8714e0d7004797d7ccf5843
Sanpa33 <127140452+Sanpa33@users.noreply.github.com>
2025-04-11 21:10:38 -0300

    Agrega punto 4 ejercicio 7

--- Practicas/Practica4/practica4.hs
+++ Practicas/Practica4/practica4.hs
@@ -34,3 +34,23 @@ medioFact :: Integer -> Integer
 medioFact 0 = 1
 medioFact 1 = 1
 medioFact n = n * medioFact(n-2) 
+
+-- Ejercicio 6
+
+sumaDigitos :: Integer -> Integer 
+sumaDigitos n | n < 10 = n
+              | n > 10 = mod n 10 + sumaDigitos(div n 10)
+
+--Ejercicio 7 
+
+ultimoDigito :: Integer -> Integer
+ultimoDigito n = mod n 10
+
+sacarUltimoDigito :: Integer -> Integer
+sacarUltimoDigito n = div n 10
+
+todosDigitosIguales :: Integer -> Bool
+todosDigitosIguales n  | n < 10 = True
+                       | n > 10 = ultimoDigito n == ultimoDigito(sacarUltimoDigito n) && todosDigitosIguales (sacarUltimoDigito n)
+
+
