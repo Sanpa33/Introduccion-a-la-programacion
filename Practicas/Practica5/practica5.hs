35d8185c6b6c81506b540174700024a4350a2282
Sanpa33 <127140452+Sanpa33@users.noreply.github.com>
2025-04-19 22:01:25 -0300

    Agrega ej 1 completo y ej 2.2 y 2.2

---
+++ Practicas/Practica5/practica5.hs
@@ -0,0 +1,38 @@
+-- Ejericicio 1 - 1)
+
+longitud :: [t] -> Integer
+longitud [] = 0
+longitud [x] = 1
+longitud (x:xs) = 1 + longitud xs
+
+-- Ejercicio 1 - 2)
+
+ultimo :: [t] -> t
+ultimo [x] = x
+ultimo (_:xs) = ultimo xs
+
+-- Ejercicio 1 - 3)
+
+principio :: [t] -> t
+principio [x] = x
+principio (x:xs) = x
+
+-- Ejercicio 1 - 4)
+
+reverso :: [t] -> [t] 
+reverso [x] = [x]
+reverso (x:xs) = reverso xs ++ [x] 
+
+--Ejercicio 2 - 1)
+
+pertenece :: (Eq t) => t -> [t] -> Bool
+pertenece n [] = False
+pertenece n (x:xs) = n == x || pertenece n xs
+
+--Ejercicio 2  -
+
+todosIguales :: (Eq t) => [t] -> Bool
+todosIguales [] = True
+todosIguales [_] = True
+todosIguales (x:y:xs) = x == y && todosIguales (y:xs)
+
